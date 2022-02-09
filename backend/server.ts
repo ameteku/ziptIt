5d64cfbb6d7d05b40e1d1bdd5dcc0cf4938e840f
Michael Ameteku <michaelameteku@gmail.com>
2022-02-09 00:57:25 -0500

    Set up some dependencies

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,13 @@
-//nothing yet
+import express from 'express';
+
+const server = express();
+
+
+server.get('test', (req, res)=> {
+    res.send("Hiiii success my boy");
+});
+
+
+server.listen(8000, ()=> {
+    console.log("Listening on 8000");
+})
