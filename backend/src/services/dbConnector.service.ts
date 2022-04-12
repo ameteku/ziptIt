6dfce7f31ed40c6f9afee742d6e11ddde9eb6786
Michael Ameteku <michaelameteku@gmail.com>
2022-04-11 22:57:37 -0400

    change firebase service path

--- backend/src/services/dbConnector.service.ts
+++ backend/src/services/dbConnector.service.ts
@@ -7,10 +7,10 @@ export default class DBConnector {
     private db: FirebaseFirestore.Firestore;
 
     constructor() {
-        //const serviceAccount = require('../dist/zipit-23932-a77e956dd224.json');
+        const serviceAccount = require('../../zipit-23932-a77e956dd224.json');
 
         initializeApp({
-            credential: cert('../dist/zipit-23932-a77e956dd224.json')
+            credential: cert(serviceAccount)
         });
         this.db = getFirestore();
     }
