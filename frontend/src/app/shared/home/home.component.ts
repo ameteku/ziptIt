503740397b94090fc64ca63854fdaec7b1bebf81
Millhouse32 <nlm82@uakron.edu>
2022-04-14 10:52:28 -0400

    Add sign in modal

--- frontend/src/app/shared/home/home.component.ts
+++ frontend/src/app/shared/home/home.component.ts
@@ -9,12 +9,16 @@ import { ModalService } from '../../_modal';
 export class HomeComponent implements OnInit {
   title: string;
   description: string;
+  username: string;
+  password: string;
 
   constructor(private modalService: ModalService) { }
 
   ngOnInit(): void {
     this.title = "Enter a title here";
     this.description = "Enter a description here";
+    this.username = "";
+    this.password = "";
   }
   openModal(id: string) {
     this.modalService.open(id);
@@ -22,6 +26,13 @@ export class HomeComponent implements OnInit {
 
 closeModal(id: string) {
     this.modalService.close(id);
+}
+
+signIn(){
+  console.log(this.username);
+  console.log(this.password);
+  this.username = "";
+  this.password = "";
 }
 
 }
