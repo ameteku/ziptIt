bc8e9f6d712b171a33d2ac95b36d2b1303d7523c
Millhouse32 <nlm82@uakron.edu>
2022-04-14 11:58:55 -0400

    begin work on sign in function

--- frontend/src/app/shared/home/home.component.ts
+++ frontend/src/app/shared/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../../_modal';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from './SignIn';
 
 @Component({
   selector: 'app-home',
@@ -11,12 +13,24 @@ export class HomeComponent implements OnInit {
   description: string;
   username: string;
   password: string;
+  email: string;
+  url: string;
+  respData: User;
+  httpOptions;
 
-  constructor(private modalService: ModalService) { }
+  constructor(private modalService: ModalService, private http: HttpClient) {
+     this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      }),
+      observe: 'response'
+  };
+   }
 
   ngOnInit(): void {
-    this.title = "Enter a title here";
-    this.description = "Enter a description here";
+    this.url = 'https://zipit-backend.herokuapp.com/login';
+    this.title = "";
+    this.description = "";
     this.username = "";
     this.password = "";
   }
@@ -26,13 +40,29 @@ export class HomeComponent implements OnInit {
 
 closeModal(id: string) {
     this.modalService.close(id);
+    this.username = "";
+    this.password = "";
 }
 
 signIn(){
   console.log(this.username);
   console.log(this.password);
+  this.http.post<User>(this.url, {"username": this.username, "password": this.password}).subscribe(data => { console.log(data);
+    this.delay(2000);
+    this.respData = data});
+  
+  console.log(this.respData);
+}
+
+signUp(){
+  console.log(this.username);
+  console.log(this.password);
   this.username = "";
   this.password = "";
+}
+
+delay(ms: number) {
+  return new Promise( resolve => setTimeout(resolve, ms) );
 }
 
 }
