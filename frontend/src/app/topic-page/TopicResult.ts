836fe7c4ade515d32b2723b4ab43e8ba0e8cdfb1
Millhouse32 <nlm82@uakron.edu>
2022-04-21 23:30:06 -0400

    Stupid git add

---
+++ frontend/src/app/topic-page/TopicResult.ts
@@ -0,0 +1,9 @@
+export interface TopicResult{
+    id: string;
+    topicId: string;
+    classId: string;
+    ratings: string[];
+    actualLink: string;
+    description: string;
+    title: string;
+}
